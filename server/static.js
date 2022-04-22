e61ce29aa4af79caf8c43899afbf9324dfcf0beb
IgorNovozhilov <maildrakon@gmail.com>
2022-04-23 00:37:06 +0300

    fastify-caching

--- server/static.js
+++ server/static.js
@@ -1,11 +1,13 @@
 import fastifyPlugin from 'fastify-plugin'
-import fastifyCompress from 'fastify-compress'
+// import fastifyCompress from 'fastify-compress'
 import fastifyStatic from 'fastify-static'
+import fastifyCaching from 'fastify-caching'
 import { resolve } from 'path'
 
 export default fastifyPlugin(function routes(app, opts, done) {
   app
-    .register(fastifyCompress)
+    .register(fastifyCaching)
+    // .register(fastifyCompress)
     .register(fastifyStatic, {
       root: resolve('client'),
       prefix: '/'
